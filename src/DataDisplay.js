b7db8e1924175fa6a2b543460fa2a5884f939a42
Nguyễn Quốc Hùng <107848000+HungNguyen20207677@users.noreply.github.com>
2024-05-04 23:43:35 +0700

    update Gpm

--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -1,118 +1,180 @@
-import React from 'react';
-import { Button, Descriptions, Tag } from 'antd';
-import calculatorService from './service';
+import React from "react";
+import { Button, Descriptions } from "antd";
+import calculatorService from "./service";
 
 const DataDisplay = () => {
   const dataResultString = localStorage.getItem("resultData");
   const dataResult = dataResultString ? JSON.parse(dataResultString) : {};
-  console.log(dataResult)
+  console.log(dataResult);
 
   const TAW = dataResult.TAWpoint;
   const TBF = dataResult.TBFpoint;
   const UUCP = dataResult.TAWpoint + dataResult.TBFpoint;
-  const TCF = 0.6 + (0.01*dataResult.TFWpoint);
-  const EF = 1.4 - 0.03*dataResult.EFWpoint;
+  const TCF = 0.6 + 0.01 * dataResult.TFWpoint;
+  const EF = 1.4 - 0.03 * dataResult.EFWpoint;
   const AUCP = UUCP * TCF * EF;
   const P = dataResult.Ppoint;
-  const E = 10/6 * AUCP;
-  const H = (dataResult.AVGSalaryMonth/20)/8;
+  const E = (10 / 6) * AUCP;
+  const H = dataResult.AVGSalaryMonth / 20 / 8;
   const G = 1.4 * E * P * H;
+  const C = (G * 65) / 100;
+  const TL = ((G + C) * 6) / 100;
+  const Gpm = G + C + TL;
 
   const items = [
     {
-      label: 'I. Tính điểm trường hợp sử dụng (Usecase)',
+      label: "I. Tính điểm trường hợp sử dụng (Usecase)",
       span: {
-          xs: 2,
-          sm: 2,
-          md: 2,
-          lg: 2,
-          xl: 2,
-          xxl: 2,
-      },
-        children: (
-          <>
-            1. Điểm actor (TAW): <b>{TAW ? TAW.toFixed(0) : 0}</b>
-            <br />
-            2. Điểm usecase (TBF): <b>{TBF ? TBF.toFixed(0) : 0}</b>
-            <br />
-            3. Tính điểm UUCP (UUCP = TAW + TBF): <b>{UUCP ? UUCP.toFixed(0) : 0}</b>
-            <br />
-            4. Hệ số phức tạp về KT-CN (TCF) (TCF = 0,6 + (0,01 x TFW)): <b>{TCF ? TCF.toFixed(2) : 0}</b>
-            <br />
-            5. Hệ số phức tạp về môi trường (EF) (EF = 1,4 + (-0,03 x EFW)):  <b>{EF ? EF.toFixed(3) : 0}</b>
-            <br />
-            6. Tính điểm AUCP (AUCP = UUCP x TCF x EF): <b>{AUCP ? AUCP.toFixed(3) : 0}</b>
-          </>
-        ),
-    },
-    {
-      label: 'II. Nội suy thời gian lao động (P)',
-      span: {
-          xs: 2,
-          sm: 2,
-          md: 2,
-          lg: 2,
-          xl: 2,
-          xxl: 2,
-      },
-        children: (
-          <>
-            Giá trị (P : người/giờ/AUCP): <b>{P ? P.toFixed(0) : 0}</b>
-          </>
-        ),
-    },
-    {
-      label: 'III. Giá trị nỗ lực thực tế (E)',
-      span: {
-          xs: 2,
-          sm: 2,
-          md: 2,
-          lg: 2,
-          xl: 2,
-          xxl: 2,
-      },
-        children: (
-          <>
-            Giá trị (E = 10/6 x AUCP): <b>{E ? E.toFixed(3) : 0}</b>
-          </>
-        ),
-    },
-    {
-      label: 'IV. Mức lương lao động bình quân (H)',
-      span: {
-          xs: 2,
-          sm: 2,
-          md: 2,
-          lg: 2,
-          xl: 2,
-          xxl: 2,
-      },
-        children: (
-          <>
-            Giá trị (H: người/giờ): <b>{H ? calculatorService.displayVNDType(H.toFixed(0)) : 0}</b>
-          </>
-        ),
-    },
-    {
-      label: 'V. Giá trị phần mềm nội bộ (G)',
-      span: {
-          xs: 2,
-          sm: 2,
-          md: 2,
-          lg: 2,
-          xl: 2,
-          xxl: 2,
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+        xl: 2,
+        xxl: 2,
       },
       children: (
-          <>
-            Giá trị (G = 1,4 x E x P x H): <b>{G ? calculatorService.displayVNDType(G.toFixed(0)) : 0} đồng</b>
-          </>
+        <>
+          1. Điểm actor (TAW): <b>{TAW ? TAW.toFixed(0) : 0}</b>
+          <br />
+          2. Điểm usecase (TBF): <b>{TBF ? TBF.toFixed(0) : 0}</b>
+          <br />
+          3. Tính điểm UUCP (UUCP = TAW + TBF):{" "}
+          <b>{UUCP ? UUCP.toFixed(0) : 0}</b>
+          <br />
+          4. Hệ số phức tạp về KT-CN (TCF) (TCF = 0,6 + (0,01 x TFW)):{" "}
+          <b>{TCF ? TCF.toFixed(2) : 0}</b>
+          <br />
+          5. Hệ số phức tạp về môi trường (EF) (EF = 1,4 + (-0,03 x EFW)):{" "}
+          <b>{EF ? EF.toFixed(3) : 0}</b>
+          <br />
+          6. Tính điểm AUCP (AUCP = UUCP x TCF x EF):{" "}
+          <b>{AUCP ? AUCP.toFixed(3) : 0}</b>
+        </>
+      ),
+    },
+    {
+      label: "II. Nội suy thời gian lao động (P)",
+      span: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+        xl: 2,
+        xxl: 2,
+      },
+      children: (
+        <>
+          Giá trị (P : người/giờ/AUCP): <b>{P ? P.toFixed(0) : 0}</b>
+        </>
+      ),
+    },
+    {
+      label: "III. Giá trị nỗ lực thực tế (E)",
+      span: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+        xl: 2,
+        xxl: 2,
+      },
+      children: (
+        <>
+          Giá trị (E = 10/6 x AUCP): <b>{E ? E.toFixed(3) : 0}</b>
+        </>
+      ),
+    },
+    {
+      label: "IV. Mức lương lao động bình quân (H)",
+      span: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+        xl: 2,
+        xxl: 2,
+      },
+      children: (
+        <>
+          Giá trị (H: người/giờ):{" "}
+          <b>{H ? calculatorService.displayVNDType(H.toFixed(0)) : 0}</b>
+        </>
+      ),
+    },
+    {
+      label: "V. Giá trị phần mềm nội bộ (G)",
+      span: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+        xl: 2,
+        xxl: 2,
+      },
+      children: (
+        <>
+          Giá trị (G = 1,4 x E x P x H):{" "}
+          <b>{G ? calculatorService.displayVNDType(G.toFixed(0)) : 0} đồng</b>
+        </>
+      ),
+    },
+    {
+      label: "VI. Chi phí chung (C)",
+      span: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+        xl: 2,
+        xxl: 2,
+      },
+      children: (
+        <>
+          Giá trị (C = G x Tỉ lệ):{" "}
+          <b>{C ? calculatorService.displayVNDType(C.toFixed(0)) : 0} đồng</b>
+        </>
+      ),
+    },
+    {
+      label: "VII. Thu thập chịu thuế tính trước (TL)",
+      span: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+        xl: 2,
+        xxl: 2,
+      },
+      children: (
+        <>
+          Giá trị (TL = ( G + C )x Tỉ lệ):{" "}
+          <b>{TL ? calculatorService.displayVNDType(TL.toFixed(0)) : 0} đồng</b>
+        </>
+      ),
+    },
+    {
+      label: "VIII. Chi phí phần mềm (Gpm)",
+      span: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+        xl: 2,
+        xxl: 2,
+      },
+      children: (
+        <>
+          Giá trị (Gpm = G + C + TL):{" "}
+          <b style={{ color: "red" }}>
+            {Gpm ? calculatorService.displayVNDType(Gpm.toFixed(0)) : 0} đồng
+          </b>
+        </>
       ),
     },
   ];
   const handleReload = () => {
-    window.location.reload()
-  }
+    window.location.reload();
+  };
 
   return (
     <>
@@ -120,29 +182,31 @@ const DataDisplay = () => {
         style={{
           top: 0,
           zIndex: 1,
-          width: '100%',
-          display: 'flex',
-          alignItems: 'right',
-          justifyContent: 'right',
+          width: "100%",
+          display: "flex",
+          alignItems: "right",
+          justifyContent: "right",
           padding: 10,
         }}
       >
-        <Button type='primary' onClick={handleReload}>Cập nhật</Button>
+        <Button type="primary" onClick={handleReload}>
+          Cập nhật
+        </Button>
       </div>
       <Descriptions
         title="Kết quả"
         bordered
         column={{
-            xs: 2,
-            sm: 2,
-            md: 2,
-            lg: 2,
-            xl: 2,
-            xxl: 2,
+          xs: 2,
+          sm: 2,
+          md: 2,
+          lg: 2,
+          xl: 2,
+          xxl: 2,
         }}
         items={items}
       />
     </>
-  )
+  );
 };
 export default DataDisplay;
--- src/service.js
+++ src/service.js
@@ -1,122 +1,130 @@
 // Tính toán giá trị TAW
 const calTAW = (data) => {
-    var result = 0;
-    for(var i = 0; i < data.length; i++){
-        result += (parseInt(data[i].quantityTAW) * data[i].weightTAW);
-    }
-    return result;
-}
+  var result = 0;
+  for (var i = 0; i < data.length; i++) {
+    result += parseInt(data[i].quantityTAW) * data[i].weightTAW;
+  }
+  return result;
+};
 
 //Tính toán giá trị BMT
 const coefficientBMT = [1, 1.2, 1.5];
-const weightBMT = [5, 10, 15]
+const weightBMT = [5, 10, 15];
 
 const calTBF = (data) => {
-    var result = 0;
-    for (var i = 0; i < data.length; i++){
-        result += ((parseInt(data[i].simpleTBF)*weightBMT[0] + parseInt(data[i].mediumTBF)*weightBMT[1] + parseInt(data[i].complicatedTBF)*weightBMT[2])*coefficientBMT[i]);
-    }
+  var result = 0;
+  for (var i = 0; i < data.length; i++) {
+    result +=
+      (parseInt(data[i].simpleTBF) * weightBMT[0] +
+        parseInt(data[i].mediumTBF) * weightBMT[1] +
+        parseInt(data[i].complicatedTBF) * weightBMT[2]) *
+      coefficientBMT[i];
+  }
 
-    return result;
-}
+  return result;
+};
 
 const calTFW = (data) => {
-    var result = 0;
-    for (var i = 0; i < data.length; i++){
-        result += (parseFloat(data[i].weightTCF)*parseFloat(data[i].ratingValueTCF));
-    }
-    return result;  
-}
+  var result = 0;
+  for (var i = 0; i < data.length; i++) {
+    result +=
+      parseFloat(data[i].weightTCF) * parseFloat(data[i].ratingValueTCF);
+  }
+  return result;
+};
 
 const calEFW = (data) => {
-    var result = 0;
-    for( var i = 0; i < data.length; i++) {
-        result += (parseFloat(data[i].weightEF) * parseFloat(data[i].ratingValueEF));
-    }
-    return result;
-}
+  var result = 0;
+  for (var i = 0; i < data.length; i++) {
+    result += parseFloat(data[i].weightEF) * parseFloat(data[i].ratingValueEF);
+  }
+  return result;
+};
 
 const calES = (data) => {
-    var result = 0;
-    for (var i = 0; i < data.length; i++) {
-        result += parseFloat(data[i].ratingExEF);
-    }
-    return result;
-}
+  var result = 0;
+  for (var i = 0; i < data.length; i++) {
+    result += parseFloat(data[i].ratingExEF);
+  }
+  return result;
+};
 
 const calP = (data) => {
-    var ESpoint = calES(data);
-    if (ESpoint < 1) {
-        return 48;
-    } else if ( ESpoint < 3 && ESpoint >= 1) {
-        return 32;
-    } else {
-        return 20;
-    }
-}
+  var ESpoint = calES(data);
+  if (ESpoint < 1) {
+    return 48;
+  } else if (ESpoint < 3 && ESpoint >= 1) {
+    return 32;
+  } else {
+    return 20;
+  }
+};
 
 const calAverageSalaryPerMonth = (data) => {
-    var sumEmployee = 0;
-    var sumSalary = 0;
+  var sumEmployee = 0;
+  var sumSalary = 0;
 
-    for(var i = 0; i < data.length; i++) {
-        sumEmployee += parseInt(data[i].numberEmployeeH);
-        sumSalary += (parseInt(data[i].wageH)*parseInt(data[i].numberEmployeeH));
-    }
+  for (var i = 0; i < data.length; i++) {
+    sumEmployee += parseInt(data[i].numberEmployeeH);
+    sumSalary += parseInt(data[i].wageH) * parseInt(data[i].numberEmployeeH);
+  }
 
-    console.log(sumSalary/sumEmployee)
+  console.log(sumSalary / sumEmployee);
 
-    return sumSalary/sumEmployee;
-}
+  return sumSalary / sumEmployee;
+};
 
 const calSumSalary = (data) => {
-    var sumSalary = 0;
+  var sumSalary = 0;
 
-    for(var i = 0; i < data.length; i++) {
-        sumSalary += (parseInt(data[i].wageH)*parseInt(data[i].numberEmployeeH));
-    }
+  for (var i = 0; i < data.length; i++) {
+    sumSalary += parseInt(data[i].wageH) * parseInt(data[i].numberEmployeeH);
+  }
 
-    return sumSalary;
-}
-
+  return sumSalary;
+};
 
 const displayVNDType = (number) => {
-    const numberString = String(number);
-    const numberArray = numberString.split('');
+  const numberString = String(number);
+  const numberArray = numberString.split("");
 
-    let result = '';
-    let count = 0;
+  let result = "";
+  let count = 0;
 
-    for (let i = numberArray.length - 1; i >= 0; i--) {
-        result = numberArray[i] + result;
+  for (let i = numberArray.length - 1; i >= 0; i--) {
+    result = numberArray[i] + result;
 
-        count++;
+    count++;
 
-        if (count === 3 && i !== 0) {
-            result = ',' + result;
-            count = 0;
-        }
+    if (count === 3 && i !== 0) {
+      result = "," + result;
+      count = 0;
     }
+  }
 
-    return result;
-}
+  return result;
+};
 
+// const calGeneralCost = (data) => {
+//     const GValue = data.GValue;
+//     return
+// }
 
 const randomKey = () => {
-    return Math.round(Math.random()*1000000)
-}
+  return Math.round(Math.random() * 1000000);
+};
 
 export const calculatorService = {
-    calTAW,
-    calTBF,
-    calTFW,
-    calEFW,
-    calES,
-    calP,
-    calAverageSalaryPerMonth,
-    calSumSalary,
-    displayVNDType,
-    randomKey
-}
+  calTAW,
+  calTBF,
+  calTFW,
+  calEFW,
+  calES,
+  calP,
+  calAverageSalaryPerMonth,
+  calSumSalary,
+  displayVNDType,
+  randomKey,
+};
 
 export default calculatorService;
